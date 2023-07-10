5a2bba7d2a3361ccebaaed5a37cdb7e7b072b033
unknown <cruz.chesarie@gmail.com>
2023-07-10 11:09:41 +0800

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <task-list></task-list>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import TaskList from './components/tasks/TaskList.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    TaskList,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
